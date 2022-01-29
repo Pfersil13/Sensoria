eb4f348f19ed593144e9c03b0ebc9efe344230c4
Pablo Fernández <pafernandez.silva@gmail.com>
2022-01-29 20:57:43 +0100

    Full record

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,38 @@
 #include <Arduino.h>
-
 #include <Mic.h>
 
 // Remember which mode we're doing
-int mode = 0;  // 0=stopped, 1=recording, 2=playing
+int mode = -2;  // 0=stopped, 1=recording, 2=playing
 const int Button_1 = 32;
+const int Button_2 = 33;
 bool state = 0;
-
+bool pulseState = 1;
 void ISR(){
-
-stopRecording();
-delay(5000);
+  pulseState = 0;
+}
+void ISR_2(){
+  state = 0;
 }
 void setup() {
   setupAudio();
   setUpSD();
   pinMode(Button_1, INPUT_PULLUP);
+  pinMode(Button_2, INPUT_PULLUP);
 Serial.begin(115200);
 attachInterrupt(digitalPinToInterrupt(Button_1), ISR, RISING);
-mode = 0;
-pulseTrain( 0.05, 10000, 10000, 1000);
+attachInterrupt(digitalPinToInterrupt(Button_2), ISR_2, RISING);
+//float audible = pulseTrain( 0.1, 1000, 1000, 1000);
+
+
 }
 
 void loop() {
-Serial.println(".");
-
+if(pulseState == 0){
+float audible = pulseTrain( 0.1, 1000, 1000, 1000);
+Serial.print("Gain: ");
+Serial.println(audible);
+//stopRecording();
+rebote(audible, 3000, 500, 5000, 1000);
+}
 }
 
